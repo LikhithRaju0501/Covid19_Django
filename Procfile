09606a9d196916cd04a1b0fe7202c9c7b1da41ef
Likhith Raju <likhithgraju@gmail.com>
2020-04-06 18:52:52 +0530

    Added Base

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coronaweb.wsgi
--- coronaweb/settings.py
+++ coronaweb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
